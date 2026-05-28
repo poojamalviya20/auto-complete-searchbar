a98680753b2757a4053177b8cbe592706b04901a
Pooja Malviya <pooja.malviya57@gmail.com>
2026-05-28 18:27:31 +0000

    auto complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
+import { useState } from "react";
 import "./styles.css";
+import { useEffect } from "react";
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+
+  const fetchData = async () => {
+    console.log("api call");
+    const data = await fetch("https://dummyjson.com/recipes/search?q=" + input);
+    const json = await data.json();
+    setResults(json?.recipes);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(fetchData, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Autocomplete Search Bar</h1>
+      <div>
+        <input
+          type="text"
+          className="search-input"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onFocus={() => setShowResult(true)}
+          onBlur={() => setShowResult(false)}
+        />
+        {showResult && (
+          <div className="results-container">
+            {results.map((r) => (
+              <span className="result" key={r.id}>
+                {" "}
+                {r.name}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
